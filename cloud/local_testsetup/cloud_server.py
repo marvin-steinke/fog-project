9e193be05ed85f3653762d91aa5f5e5f61798cdd
niklasfomin <niklas.fomin@campus.tu-berlin.de>
2023-06-28 22:20:20 +0200

    edge-cloud conn. works, resend if failure wip

--- cloud/local_testsetup/cloud_server.py
+++ cloud/local_testsetup/cloud_server.py
@@ -6,23 +6,48 @@ import redis
 
 logging.basicConfig(level=logging.INFO)
 
-# Assign the redis cache:
-cache = redis.Redis(import redis
+cache_host = '127.0.0.1'
+redis_port = 6379
+redis_db = 0
 
-# Connect to Redis
-r = redis.Redis(host='redis', port=6379)
+def establish_cache_connection():
+    """
+    Establishes a connection to the Redis cache server.
+
+    Returns:
+        redis.Redis: Redis cache connection object.
+
+    Raises:
+        redis.RedisError: If there is an error connecting to the Redis cache server.
+    """
+    try:
+        cache = redis.Redis(host=cache_host, port=redis_port, db=redis_db)
+        logging.info("Connected to Redis cache server.")
+        return cache
+    except redis.RedisError as e:
+        logging.error(f"Error connecting to Redis cache server: {e}")
+        raise
+
+# Establish connection to Redis cache server
+cache = establish_cache_connection()
 
 async def receive_heartbeat():
+    """
+    Receives heartbeat messages from edge servers.
+    """
     with pynng.Pair0() as heartbeat_socket:
         heartbeat_socket.listen('tcp://localhost:63270')
         while True:
             message = await heartbeat_socket.arecv()
             if message == b'heartbeat':
-                print('Received heartbeat message')
+                logging.info('Received heartbeat message')
                 await heartbeat_socket.asend(b'ack')
-                print('Sent ack message')
-                
+                logging.info('Sent ack message')
+
 async def receive_data():
+    """
+    Receives data from edge servers.
+    """
     with pynng.Sub0() as data_socket:
         data_socket.listen('tcp://localhost:63271')
         data_socket.subscribe(b'')
@@ -33,31 +58,62 @@ async def receive_data():
                 id = request_data.get('id', 'Unknown')
                 node_id = request_data.get('node_id', 'Unknown')
                 average = request_data.get('average', 'Unknown')
-                print(f"Received data from edge server - id: {id}, node_id: {node_id}, average: {average}")
+                logging.info(f"Received data from edge server - id: {id}, node_id: {node_id}, average: {average}")
                 ack_data(id)
+                cache_data(id, average)
             except pynng.exceptions.TryAgain:
                 await asyncio.sleep(1)
+            except json.JSONDecodeError as e:
+                logging.error(f"Failed to decode JSON data: {e}")
+            except Exception as e:
+                logging.error(f"Error while receiving data: {e}")
 
 def ack_data(id):
+    """
+    Sends acknowledgement for received data to edge servers.
+
+    Args:
+        id (str): ID of the received data.
+    """
     with pynng.Pub0() as ack_socket:
         try:
             ack_socket.dial('tcp://localhost:63272')
-            sequence_number = f"Acknowledgement for receiv id: {id}".encode('utf-8')
+            sequence_number = f"Acknowledgement for receive id: {id}".encode('utf-8')
             ack_socket.send(sequence_number)
-            print(f"Sent acknowledgement for id: {id}")
+            logging.info(f"Sent acknowledgement for id: {id}")
+            #cache.hset(id, 'acknowledged', int(True))  
         except pynng.exceptions.TryAgain:
-            print("Connection not available yet")
-            
+            logging.error("Connection not available yet")
+        except Exception as e:
+            logging.error(f"Error while sending acknowledgement: {e}")
+
 def cache_data(id, node_average):
-    
+    """
+    Caches the received data.
+
+    Args:
+        id (str): ID of the received data.
+        node_average (float): Average value associated with the received data.
+    """
+    try:
+        cache.set(id, node_average)
+        #cache.hset(id, 'acknowledged', int(False))  
+        logging.info(f"Cached data - id: {id}, node_average: {node_average}")
+    except redis.RedisError as e:
+        logging.error(f"Error while caching data: {e}")
+    except Exception as e:
+        logging.error(f"Error while caching data: {e}")
 
 async def main():
+    """
+    Main function to run the server.
+    """
     receive_task = asyncio.create_task(receive_data())
     heartbeat_task = asyncio.create_task(receive_heartbeat())
 
     await asyncio.gather(receive_task, heartbeat_task)
 
-asyncio.run(main())
-
-
-
+try:
+    asyncio.run(main())
+except KeyboardInterrupt:
+    logging.info("KeyboardInterrupt: Stopping the server.")
--- edge/edge_server.py
+++ edge/edge_server.py
@@ -36,10 +36,17 @@ class EdgeServer:
     def __init__(self, bootstrap_servers: str, input_topic: str, output_topic: str, db_handler: dbHandler) -> None:
         
         self.logger = logging.getLogger("EdgeServer")
+        
+        # db stuff
         db_file = os.path.join(os.path.dirname(__file__), '..', 'local.db')
-        self.cloud_connected = False
         self.db_handler = db_handler
-
+        
+        # connection flags
+        self.cloud_connected = False
+        # self.sent_successfully = False
+        # self.acknowledged_successfully = False
+        
+        # simulation environment
         self.consumer = KafkaConsumer(
             input_topic,
             bootstrap_servers=bootstrap_servers,
@@ -55,6 +62,7 @@ class EdgeServer:
         self.ready = False
         self.shutdown = False
 
+
     def _consumer_thread(self) -> None:
         """Consumes messages from the input Kafka topic and stores the values
         in the data attribute."""
@@ -65,6 +73,7 @@ class EdgeServer:
                 power_value = message[0].value['power_value']
                 with self.lock:
                     self.data[node_id].append(power_value)
+
 
     def _producer_thread(self) -> None:
         """Periodically calculates averages of the power values and sends them
@@ -89,6 +98,7 @@ class EdgeServer:
 
                         values.clear()
 
+
     def connection_thread(self) -> None:
         """
         Thread that continually checks for connection and sets the cloud_connected flag.
@@ -108,11 +118,11 @@ class EdgeServer:
                 self.cloud_connected = False
                 print(f"Failed to connect to the server. Error: {e}. Retrying in 2 seconds...")
                 time.sleep(2)
-            # else:
-            #     if not prev_connection_status:
-            #         # If the connection was restored, send unsent data
-            #         self._send_unsent_data()
-            #     prev_connection_status = True
+            else:
+                if not prev_connection_status:
+                    # If the connection was restored, send unsent data
+                    self._send_unsent_data()
+                prev_connection_status = True
         time.sleep(5)
 
 
@@ -139,18 +149,21 @@ class EdgeServer:
             print(f"Error occurred while sending data to the server: {e}")
             return False
     
+    
     def _send_unsent_data(self) -> None:
         """Fetches unsent data from the local db, sends it to the cloud, and updates the status flag in the row."""
         while not self.shutdown:
             unsent_data = self.db_handler.get_unsent_power_averages()
-            for row in unsent_data:
+            unacknowledged_data = self.db_handler.get_unacknowledged_power_averages()
+            for row in unsent_data and unacknowledged_data:
                 id, node_id, average, timestamp = row
                 data = {"node_id": node_id, "average_power": average}
-                #sent_successfully = self._send_to_server(id, node_id, average)
-                # if sent_successfully:
+                sent_successfully = self._send_to_server(id, node_id, average)
+                ack_successfull = self.receive_server_acks()
+                # if sent_successfully and acknowledged_suss:
                 #     self.db_handler.update_sent_flag(id)
                 # else:
-                #     self.logger.error(f"Failed to send unsent data with id {id} to the cloud node.")
+                    #self.logger.error(f"Failed to send unsent data with id {id} to the cloud node.")
             time.sleep(3)
 
 
@@ -208,7 +221,6 @@ class EdgeServer:
         self.producer_thread.join()
         self.consumer.close()
         self.producer.close()
-        self.connection_check_thread.close()
         self.ready = False
         self.db_handler.truncate_table("power_averages")
         self.db_handler.close_connection()
