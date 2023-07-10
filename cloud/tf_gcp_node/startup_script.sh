9602b74419b72dcaf47f4c0b8710e5d767bfa2b2
marvin.steinke <marvin.steinke@campus.tu-berlin.de.de>
2023-07-10 18:20:53 +0200

    fixed startup_script and requirements

--- cloud/tf_gcp_node/startup_script.sh
+++ cloud/tf_gcp_node/startup_script.sh
@@ -1,11 +1,11 @@
 apt-get update
-apt-get install python3-pip git
+apt-get install python3-pip git -y
 cd /opt
 git clone --depth 1 -b feature/edge-cloud-client https://github.com/marvin-steinke/fog-project.git
 cp -r fog-project/cloud/gcloud_deployment server
 rm -rf fog-project
 cd server
-pip3 install -r requirements.txt --break-system-packages
+pip3 install -r requirements.txt
 # comment for video
 # python3 cloud-server.py
 cd web-client
