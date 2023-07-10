2a88160b25cfaa55d5dff095648001671dd22588
marvin.steinke <marvin.steinke@campus.tu-berlin.de.de>
2023-07-10 18:04:49 +0200

    git clone now only clones feature branch for testing

--- cloud/tf_gcp_node/startup_script.sh
+++ cloud/tf_gcp_node/startup_script.sh
@@ -1,7 +1,7 @@
 apt-get update
 apt-get install python3-pip git
 cd /opt
-git clone --depth 1 https://github.com/marvin-steinke/fog-project.git
+git clone --depth 1 -b feature/edge-cloud-client https://github.com/marvin-steinke/fog-project.git
 cp -r fog-project/cloud/gcloud_deployment server
 rm -rf fog-project
 cd server
